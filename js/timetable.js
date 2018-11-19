3057c2d259374824493b9900e565f4ae82739f49
Daniele Megna <megna.dany@gmail.com>
2018-11-19 19:50:11 +0100

    dinamically generate time table rows

--- js/timetable.js
+++ js/timetable.js
@@ -6,55 +6,31 @@ export default class TimeTable extends HTMLElement {
   }
 
   connectedCallback() {
-    this.innerHTML = `
-      <h2>Settimana ${this.startOfWeek.week()}</h2>
-      <table class="pure-table pure-table-bordered green">
-        <thead>
-          <tr>
-            <th>NOVEMBRE</th>
-            <th>Mattino</th>
-            <th>Sera</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Lun 19</td>
-            <td>Cristina</td>
-            <td>Anna</td>
-          </tr>
-          <tr>
-            <td>Mar 20</td>
-            <td>Anna</td>
-            <td>Cristina & girls</td>
-          </tr>
-          <tr>
-            <td>Mer 21</td>
-            <td>Eleonora, Cristina</td>
-            <td>Anna</td>
-          </tr>
-          <tr>
-            <td>Gio 22</td>
-            <td>Anna</td>
-            <td>Sonia</td>
-          </tr>
-          <tr>
-            <td>Ven 23</td>
-            <td>Eleonora</td>
-            <td>Cristina & girls</td>
-          </tr>
-          <tr>
-            <td>Sab 24</td>
-            <td>Anna, Monica, Erika, Elisa</td>
-            <td>Daniele, Stefania, Giona</td>
-          </tr>
-          <tr>
-            <td>Dom 25</td>
-            <td>Barbara, Laura, Elena, Cinzia</td>
-            <td>Anna, Rossana</td>
-          </tr>
-        </tbody>
-      </table>`
+    const day = this.startOfWeek.clone()
+
+    var tableHtml = `<h2>Settimana ${day.week()}</h2>`
+    tableHtml += `<table class="pure-table pure-table-bordered green">
+      <thead><tr>
+        <th>${day.format("MMMM").toUpperCase()}</th>
+        <th>Mattino</th>
+        <th>Sera</th>
+      </tr></thead>
+      <tbody>`
+
+    for(var i=0; i<7; i++) {
+      tableHtml += `
+        <tr>
+          <td>${day.format("ddd D")}</td>
+          <td>Cristina</td>
+          <td>Anna</td>
+        </tr>`
+      day.add(1, 'days')
+    }
+
+    tableHtml += `</tbody></table>`
+    this.innerHTML = tableHtml
   }
+
 
 }
 
