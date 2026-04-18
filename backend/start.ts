18e396a29df92dcec16cde3812e46416a7544e6f
Daniele Megna <megna.dany@gmail.com>
2026-04-18 16:50:30 +0200

    Trying handlebars for html templating pages

--- backend/start.ts
+++ backend/start.ts
@@ -1,4 +1,11 @@
 import http = require('http');
+import fs = require('fs');
+import path = require('path');
+import handlebars = require('handlebars');
+
+const editHtmlTemplate = handlebars.compile(
+  fs.readFileSync(path.join(__dirname, 'views', 'edit.html'), 'utf-8')
+)
 
 http.createServer((request, response) => {
   console.log(request.url)
@@ -6,6 +13,15 @@ http.createServer((request, response) => {
     case '/': {
       response.writeHead(200, { 'Content-Type': 'text/plain' })
       response.end('Hello, world!', 'utf-8')
+      break;
+    }
+    case '/edit': {
+      const data = JSON.parse(fs.readFileSync(path.join(__dirname, 'database', 'default.json'), 'utf-8'));
+      const names = data[0][0];
+
+      const html = editHtmlTemplate({ names });
+      response.writeHead(200, { 'Content-Type': 'text/html' })
+      response.end(html, 'utf-8')
       break;
     }
     default: {
