3635104fda683e588c33d538e7de2b4fb94271c6
Daniele Megna <megna.dany@gmail.com>
2026-04-25 18:27:24 +0200

    Implement addToShift to add name to json content

--- backend/usecases/add.ts
+++ backend/usecases/add.ts
@@ -1,6 +1,10 @@
 import fs from 'fs'
 import path from 'path'
 
-export function addToShift(weekNumber: number, dayOfWeek: number, shift: number, name: string, ): void {
-  console.debug('>>> add: ', {weekNumber, dayOfWeek, shift, name})
+export function addToShift(weekNumber: number, dayOfWeek: number, shift: number, name: string): void {
+  const filePath = path.join(__dirname, '..', 'database', `week_${weekNumber}.json`)
+  const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+  const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1)
+  data[dayOfWeek][shift].push(capitalizedName)
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf-8')
 }
