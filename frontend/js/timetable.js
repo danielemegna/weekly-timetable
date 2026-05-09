993f4f654fb90b350f3fdc4be9851a89c4047050
Daniele Megna <megna.dany@gmail.com>
2026-05-09 18:29:29 +0200

    Move TimeTable htmlFor function as Class function

--- frontend/js/timetable.js
+++ frontend/js/timetable.js
@@ -9,42 +9,42 @@ export default class TimeTable extends HTMLElement {
   }
 
   connectedCallback() {
-    this.outerHTML = htmlFor(this.startOfWeek, this.shifts)
+    this.outerHTML = this.buildHtml()
   }
 
-}
+  buildHtml() {
+    const day = this.startOfWeek.clone()
+    const weekNumber = this.startOfWeek.week()
 
-function htmlFor(startOfWeek, shifts) {
-  const day = startOfWeek.clone()
-  const weekNumber = startOfWeek.week()
+    const openTable = `
+      <table id="time-table" class="pure-table pure-table-bordered ${colorFromWeekNumber(day.week())}">
+        <thead><tr>
+          <th>${day.format("MM.YYYY").toUpperCase()}</th>
+          <th>Mattino</th>
+          <th>Sera</th>
+        </tr></thead>
+        <tbody>`
 
-  const openTable = `
-    <table id="time-table" class="pure-table pure-table-bordered ${colorFromWeekNumber(day.week())}">
-      <thead><tr>
-        <th>${day.format("MM.YYYY").toUpperCase()}</th>
-        <th>Mattino</th>
-        <th>Sera</th>
-      </tr></thead>
-      <tbody>`
+    const closeTable = `</tbody></table>`
 
-  const closeTable = `</tbody></table>`
+    var tableRows = ""
+    for(var i=0; i<7; i++) {
+      tableRows += `
+        <tr>
+          <td>${day.format("ddd D")}</td>
+          <td onclick="redirectToEditPage(${weekNumber}, ${i}, 0)">
+            ${this.shifts[i][0].join(", ")}
+          </td>
+          <td onclick="redirectToEditPage(${weekNumber}, ${i}, 1)">
+            ${this.shifts[i][1].join(", ")}
+          </td>
+        </tr>`
+      day.add(1, 'days')
+    }
 
-  var tableRows = ``
-  for(var i=0; i<7; i++) {
-    tableRows += `
-      <tr>
-        <td>${day.format("ddd D")}</td>
-        <td onclick="redirectToEditPage(${weekNumber}, ${i}, 0)">
-          ${shifts[i][0].join(", ")}
-        </td>
-        <td onclick="redirectToEditPage(${weekNumber}, ${i}, 1)">
-          ${shifts[i][1].join(", ")}
-        </td>
-      </tr>`
-    day.add(1, 'days')
+    return openTable + tableRows + closeTable
   }
 
-  return openTable + tableRows + closeTable
 }
 
 window.redirectToEditPage = function redirectToEditPage(weekNumber, dayOfWeek, shift) {
