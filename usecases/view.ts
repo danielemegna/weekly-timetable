0007228d6e80b288123ebc999ea89b5e856b7d09
Daniele Megna <megna.dany@gmail.com>
2026-06-27 09:33:00 +0200

    Minor refactor in getNamesFor view usecase

--- usecases/view.ts
+++ usecases/view.ts
@@ -3,14 +3,15 @@ import path from 'path'
 
 export function getNamesFor(weekNumber: number, dayOfWeek: number, shift: number): string[] {
   const data = getDatabaseFor(weekNumber)
-  const names = data[dayOfWeek][shift]
-  return names
+  return data[dayOfWeek][shift]
 }
 
 function getDatabaseFor(weekNumber: number): any {
   const dbFolder = path.join(__dirname, '..', 'database')
   const weekFilePath = path.join(dbFolder, `week_${weekNumber}.json`)
+
   if (!fs.existsSync(weekFilePath))
     fs.copyFileSync(path.join(dbFolder, 'default.json'), weekFilePath)
+
   return JSON.parse(fs.readFileSync(weekFilePath, 'utf-8'))
 }
